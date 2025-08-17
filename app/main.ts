f8d353034b23d6b0f6be54b41f113980e1e951ed
matheus-hrm <rodriguesm673@gmail.com>
2025-08-17 12:49:21 -0300

    match

--- app/main.ts
+++ app/main.ts
@@ -16,8 +16,10 @@ if (args[2] !== "-E") {
   process.exit(1);
 }
 
+// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.error("Logs from your program will appear here!");
 
+// Uncomment this block to pass the first stage
 if (matchPattern(inputLine, pattern)) {
   process.exit(0);
 } else {
